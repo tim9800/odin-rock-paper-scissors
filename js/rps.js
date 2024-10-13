9c9e094860b7692197ae11f835dc1dae1da06619
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-13 15:33:08 +1100

    Define round logic

--- js/rps.js
+++ js/rps.js
@@ -17,4 +17,67 @@ getHumanChoice = function () {
     return(choice);
 }
 
+
+playGame = function() {
+    let humanScore = 0, computerScore = 0;
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+
+    playRound = function(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        let outcome;
+        
+        if (humanChoice == computerChoice) {
+            outcome = "Draw";
+        } else if (humanChoice == "rock") {
+            switch (computerChoice) {
+                case "paper":
+                    outcome = "Lose";
+                    break;
+                case "scissors":
+                    outcome = "Win";
+                    break;
+            }
+        } else if (humanChoice == "paper") {
+            switch (computerChoice) {
+                case "rock":
+                    outcome = "Win";
+                    break;
+                case "scissors":
+                    outcome = "Lose";
+                    break;
+            }
+        } else if (humanChoice == "scissors") {
+            switch (computerChoice) {
+                case "rock":
+                    outcome = "Lose";
+                    break;
+                case "paper":
+                    outcome = "Win";
+                    break;
+            }
+        }
+    
+        let message;
+        switch (outcome) {
+            case "Draw":
+                message = `It's a draw! You and the computer both chose ${humanChoice}`;
+                break;
+        
+            case "Win":
+                message = `You win! Your choice (${humanChoice}) beats the computer's choice (${computerChoice})`;
+                humanScore++;
+                break;
+    
+            case "Lose":
+                message = `You lose! Your choice (${humanChoice}) is beaten by the computer's choice (${computerChoice})`;
+                computerScore++;
+                break;
+    
+        }
+        
+        console.log(message);
+    }
 }
+
